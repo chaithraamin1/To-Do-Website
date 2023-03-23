c25489cff3f5b54d5237d291b98e59b3eebec1f8
chai <chaithra.amin98@gmail.com>
2023-03-23 13:19:33 +0530

    to-do list website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list-app';
+  ngOnInit(){
+    var listContainer:any=document.getElementById("list-container");
+    listContainer.addEventListener("click",function(e:any){
+      debugger
+         if(e.target.tagName=="LI")
+         e.target.classList.toggle("checked")
+         else if(e.target.tagName=="SPAN")
+         e.target.parentElement.remove();
+    },false);
+  }
+  addBtn(){
+    var listContainer:any=document.getElementById("list-container");
+    var inputBox:any=document.getElementById("input-box");
+    if(inputBox.value==''){
+      alert("must enter something")
+    }
+    else{
+      let li=document.createElement("li");
+      li.innerHTML=inputBox.value;
+      listContainer.appendChild(li);
+      let span=document.createElement("span");
+      span.innerHTML="\u00d7";
+      li.appendChild(span)
+    }
+    inputBox.value='';
+    
+  }
+ 
+
+
 }
